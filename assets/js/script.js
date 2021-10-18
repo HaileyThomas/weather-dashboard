b01ee381547af3456e118c97657c39c815729f98
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-18 16:28:44 -0700

    show current results

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,8 @@ var currentTime;
 var currentTemp;
 var currentHumidity;
 var currentWind;
-var currentUv;
+var currentUvi;
+var currentIcon;
 
 // FORM FUNCTION
 var formSubmitHandler = function (event) {
@@ -62,17 +63,54 @@ var getWeather = function () {
     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=" + "hourly,minutely" + "&units=" + "imperial" + "&appid=" + "d50140606331b5f0875df8b66c236b78";
     // fetch weather api
     fetch(weatherApiUrl)
+        // get and parse response
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            // testing logs
             console.log(data);
             console.log(data.current);
             console.log(data.current.temp);
             console.log(data.current.dt);
+            // set current variables
+            currentTime = data.current.dt;
+            console.log("current time: " + currentTime);
             currentTemp = data.current.temp;
             console.log("current temp: " + currentTemp);
+            currentHumidity = data.current.humidity;
+            console.log("current humidity: " + currentHumidity);
+            currentWind = data.current.wind_speed;
+            console.log("current wind speed: " + currentWind);
+            currentUvi = data.current.uvi;
+            console.log("current uvi: " + currentUvi);
+            currentIcon = data.current.weather.icon;
+            console.log("current icon: " + currentIcon);
+            // run display current weather function
+            displayCurrent();
         });
+};
+
+// DISPLAY CURRENT WEATHER
+var displayCurrent = function () {
+    console.log("function working");
+    // create container
+    var currentContainer = document.createElement("div");
+    currentContainer.className = "card m-3";
+    resultsEl.appendChild(currentContainer);
+    // create results header and div
+    var currentHeader = document.createElement("h2");
+    currentHeader.className = "card-header bg-primary text-white";
+    currentHeader.textContent = city;
+    currentContainer.appendChild(currentHeader);
+    var currentResults = document.createElement("div");
+    currentResults.className = "card-body d-flex flex-column";
+    currentContainer.appendChild(currentResults);
+    // show current results
+    var showCurrentResults = document.createElement("p");
+    showCurrentResults.className = "fs-4";
+    showCurrentResults.innerHTML = "<b>Current Temperature:</b> " + currentTemp + " °F <br/> <b>Current Humidity:</b> " + currentHumidity + " % <br /> <b>Current Wind Speed:</b> " + currentWind + " MPH <br /> <b>UV Index:</b> " + currentUvi;
+    currentResults.appendChild(showCurrentResults);
 };
 
 // SAVE CITY FUNCTION
@@ -99,7 +137,6 @@ var updateHistory = function () {
 // DISPLAY HISTORY FUNCTION
 var displayHistory = function () {
     for (i = 0; i < listHistory.length; i++) {
-        console.log("item: " + listHistory[i]);
         // create list element
         var li = document.createElement("li");
         li.className = "list-group-item";
